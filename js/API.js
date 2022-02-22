e2a357792611f2e65c8fdc794173cb9b1919aac0
Sobuj <sobujkumar424@gmail.com>
2022-02-22 19:26:29 +0600

    Data-lode

--- js/API.js
+++ js/API.js
@@ -1,5 +1,21 @@
+// Lode Data
 function lodeData(){
     fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(response => response.json())
     .then(json => console.log(json))
+}
+// Lode User
+function lodeUser(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(Data => displayUser(Data))
+}
+// Lode Albums
+function lodeAlbums(){
+    fetch('https://jsonplaceholder.typicode.com/albums')
+    .then(res => res.json())
+    .then(Data => console.log(Data))
+}
+function displayUser(Data){
+    console.log(Data);
 }
