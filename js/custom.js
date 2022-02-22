4db0f2f3de2d70fafc054477c15a07b7bf1b73ba
Sobuj <sobujkumar424@gmail.com>
2022-02-22 18:33:15 +0600

    json-stringify

--- js/custom.js
+++ js/custom.js
@@ -3,4 +3,22 @@ let user = {
     Id:111,
     job:'Gurafera'
 };
-console.log(user)
+let stringified = JSON.stringify(user);
+// console.log(user);
+// console.log(stringified)
+//
+let shop = {
+    name:'play store',
+    address:'vuter goli',
+    products:['boll','jarshi','bat','butt'],
+    oner:{
+        name:'neimar',
+        profession:'player'
+    },
+    isExpensive: false
+};
+let shopStringified = JSON.stringify(shop);
+console.log(shopStringified);
+//
+let converted = JSON.parse(shopStringified);
+console.log(converted);
